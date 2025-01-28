c65728c2b80fb065ff5a36b09d9af1ec3788e37f
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2025-01-28 17:18:31 +0100

    hero edit

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -49,11 +49,14 @@ const MainPage = () => {
 
       <main className="px-[5%]" id="explore">
         <div className="2xl:container mx-auto">
-          <h1 className="headerFont text-center flex justify-between text-[2rem] lg:text-[7rem] tracking-[1rem] lg:tracking-[2.5rem]">
-            {/* <span>Prince</span>
+          <div className="flex justify-center">
+            <h1 className="headerFont w-fit text-center flex text-[2rem] lg:text-[7rem] tracking-[.5rem] lg:tracking-[2.5rem]">
+              {/* <span>Prince</span>
             <span>Rufus</span> */}
-            <p>JPEGBYPRINCE</p>
-          </h1>
+              <p>JPEGBYPRINCE</p>
+            </h1>
+          </div>
+
           <div
             className="relative h-[35rem] lg:h-[45rem] w-full rounded-md"
             style={{
